c18712a74a321768ba79f927059af66e11212d4f
Gaëtan de Menten <gdementen@gmail.com>
2015-11-24 11:42:32 +0100

    prepare for 0.2.6

--- larray/core.py
+++ larray/core.py
@@ -1,7 +1,7 @@
 # -*- coding: utf8 -*-
 from __future__ import absolute_import, division, print_function
 
-__version__ = "0.2.5"
+__version__ = "0.2.6"
 
 __all__ = [
     'LArray', 'Axis', 'AxisCollection', 'ValueGroup',
