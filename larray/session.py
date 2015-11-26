86708858d1ba795aac9ea99748b06d56b3c783ce
Gaëtan de Menten <gdementen@gmail.com>
2015-11-26 12:57:21 +0100

    implemented filter, load, dump, names on Session

--- larray/session.py
+++ larray/session.py
@@ -1,15 +1,44 @@
 from __future__ import absolute_import, division, print_function
 
+import os
+
+from pandas import ExcelWriter
+from larray.core import LArray, read_csv, read_excel, read_hdf
+
 
 def check_pattern(k, pattern):
     return k.startswith(pattern)
 
 
+def read_multi_csv(fname, name):
+    return read_csv(fname)
+
+
 class Session(object):
     def __init__(self, *args, **kwargs):
+        # self._objects = {}
         object.__setattr__(self, '_objects', {})
-                           # self._objects = {}
-        self.add(*args, **kwargs)
+
+        if len(args) == 1 and not hasattr(args[0], 'name') and not \
+                isinstance(args[0], dict):
+            # assume we have an iterable of tuples
+            arg = args[0]
+            for k, v in arg:
+                self[k] = v
+        elif len(args) == 1 and isinstance(args[0], dict):
+            self.add(**args[0])
+        else:
+            self.add(*args, **kwargs)
+
+    # XXX: behave like a dict and return keys instead?
+    def __iter__(self):
+        return iter(self._objects[k] for k in self.names)
+
+    def add(self, *args, **kwargs):
+        for arg in args:
+            self[arg.name] = arg
+        for k, v in kwargs.items():
+            self[k] = v
 
     def __getitem__(self, key):
         return self._objects[key]
@@ -23,32 +52,96 @@ class Session(object):
     def __setattr__(self, key, value):
         self._objects[key] = value
 
-    # TODO: implement __iter__
-    # def load(self, fname, names):
-    #     # TODO: support path + *.csv
-    #     funcs = {'.h5': read_hdf, '.xls': read_excel}
-    #     func = funcs[ext]
-    #     for name in names:
-    #         self[name] = func(fname, name)
-    #
-    # def dump(self, fname, fmt='.h5'):
-    #     func = LArray.to_hdf
-    #     for obj in self.objects(kind=LArray):
-    #         func(obj, fname, name)
+    def load(self, fname, names):
+        """Load LArray objects from a file.
 
-    # TODO: rename to filter and return another Session
-    def objects(self, pattern=None, kind=None):
-        keys = self._objects.keys()
+        Parameters
+        ----------
+        fname : str
+            Path to the file.
+        names : list of str
+            List of arrays to load.
+        """
+        # TODO: add support for names=None for .h5 or .xlsx => all in file
+        # TODO: support path + *.csv
+        funcs = {'.h5': read_hdf, '.xls': read_excel, '.xlsx': read_excel,
+                 '.csv': read_multi_csv}
+        _, ext = os.path.splitext(fname)
+        func = funcs[ext]
+        for name in names:
+            self[name] = func(fname, name)
+
+    def dump(self, fname, fmt='auto'):
+        """Dumps all LArray objects to a file.
+
+        Parameters
+        ----------
+        fname : str
+            Path for the dump.
+        fmt : str, optional
+            Dump to the `fmt` format. Defaults to 'auto' (guess from
+            filename).
+        """
+        if fmt == 'auto':
+            _, ext = os.path.splitext(fname)
+            fmt = ext.strip('.')
+        funcs = {'h5': self.dump_hdf, 'hdf': self.dump_hdf,
+                 'xlsx': self.dump_excel, 'xls': self.dump_excel,
+                 'csv': self.dump_csv}
+        funcs[fmt](fname)
+
+    def dump_hdf(self, fname, *args, **kwargs):
+        for k, v in self.filter(kind=LArray).items():
+            v.to_hdf(fname, k, *args, **kwargs)
+
+    def dump_excel(self, fname, *args, **kwargs):
+        writer = ExcelWriter(fname)
+        for k, v in self.filter(kind=LArray).items():
+            v.to_excel(writer, k, *args, **kwargs)
+        writer.save()
+
+    def dump_csv(self, path, *args, **kwargs):
+        for k, v in self.filter(kind=LArray).items():
+            v.to_csv(os.path.join(path, '{}.csv'.format(k)), *args, **kwargs)
+
+    def filter(self, pattern=None, kind=None):
+        """Return a new Session with objects which match some criteria.
+
+        Parameters
+        ----------
+        pattern : str, optional
+            Only keep objects whose key match `pattern`.
+        kind : type, optional
+            Only keep objects which are instances of type `kind`.
+
+        Returns
+        -------
+        Session
+            The filtered session.
+        """
         if pattern is not None:
-            keys = [k for k in keys if check_pattern(k, pattern)]
-        objects = [self._objects[k] for k in sorted(keys)]
-        if kind is not None:
-            return [obj for obj in objects if isinstance(obj, kind)]
+            items = [(k, self._objects[k]) for k in self._objects.keys()
+                     if check_pattern(k, pattern)]
         else:
-            return objects
+            items = self._objects.items()
+        if kind is not None:
+            return Session([(k, v) for k, v in items if isinstance(v, kind)])
+        else:
+            return Session(items)
 
-    def add(self, *args, **kwargs):
-        for arg in args:
-            self[arg.name] = arg
-        for k, v in kwargs.items():
-            self[k] = v
+    # XXX: would having an option/another function for returning this unsorted
+    # be any useful?
+    @property
+    def names(self):
+        return sorted(self._objects.keys())
+
+    # XXX: sorted?
+    def values(self):
+        return self._objects.values()
+
+    # XXX: sorted?
+    def items(self):
+        return self._objects.items()
+
+    def __repr__(self):
+        return 'Session({})'.format(', '.join(self.names))
--- larray/tests/test_session.py
+++ larray/tests/test_session.py
@@ -3,14 +3,17 @@ from __future__ import absolute_import, division, print_function
 from unittest import TestCase
 import unittest
 
-from larray import Session, Axis
+from larray import Session, Axis, LArray, ndrange
 
 
 class TestSession(TestCase):
     def setUp(self):
         self.a = Axis('a', [])
         self.b = Axis('b', [])
-        self.session = Session(self.a, self.b, c='c', d={})
+        self.e = ndrange((2, 3))
+        self.f = ndrange((3, 2))
+        self.session = Session(self.a, self.b, c='c', d={},
+                               e=self.e, f=self.f)
 
     def test_getitem(self):
         s = self.session
@@ -21,8 +24,8 @@ class TestSession(TestCase):
 
     def test_setitem(self):
         s = self.session
-        s['e'] = 'e'
-        self.assertEqual(s['e'], 'e')
+        s['g'] = 'g'
+        self.assertEqual(s['g'], 'g')
 
     def test_getattr(self):
         s = self.session
@@ -33,8 +36,8 @@ class TestSession(TestCase):
 
     def test_setattr(self):
         s = self.session
-        s.f = 'f'
-        self.assertEqual(s.f, 'f')
+        s.g = 'g'
+        self.assertEqual(s.g, 'g')
 
     def test_add(self):
         s = self.session
@@ -43,16 +46,40 @@ class TestSession(TestCase):
         self.assertEqual(s.g, g)
         self.assertEqual(s.h, 'h')
 
-    def test_objects(self):
+    def test_iter(self):
+        self.assertEqual(list(self.session), [self.a, self.b, 'c', {},
+                                              self.e, self.f])
+
+    def test_filter(self):
         s = self.session
         s.ax = 'ax'
-        self.assertEqual(s.objects(), [self.a, 'ax', self.b, 'c', {}])
-        self.assertEqual(s.objects('a'), [self.a, 'ax'])
-        self.assertEqual(s.objects('a', dict), [])
-        self.assertEqual(s.objects('a', str), ['ax'])
-        self.assertEqual(s.objects('a', Axis), [self.a])
-        self.assertEqual(s.objects(kind=Axis), [self.a, self.b])
-        self.assertEqual(s.objects(kind=dict), [{}])
+        self.assertEqual(list(s.filter()), [self.a, 'ax', self.b, 'c', {},
+                                            self.e, self.f])
+        self.assertEqual(list(s.filter('a')), [self.a, 'ax'])
+        self.assertEqual(list(s.filter('a', dict)), [])
+        self.assertEqual(list(s.filter('a', str)), ['ax'])
+        self.assertEqual(list(s.filter('a', Axis)), [self.a])
+        self.assertEqual(list(s.filter(kind=Axis)), [self.a, self.b])
+        self.assertEqual(list(s.filter(kind=LArray)), [self.e, self.f])
+        self.assertEqual(list(s.filter(kind=dict)), [{}])
+
+    def test_names(self):
+        s = self.session
+        self.assertEqual(s.names, ['a', 'b', 'c', 'd', 'e', 'f'])
+        # add them in the "wrong" order
+        s.add(h='h')
+        s.add(g='g')
+        self.assertEqual(s.names, ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'])
+
+    def test_dump(self):
+        self.session.dump('test.h5')
+        self.session.dump('test.xlsx')
+        self.session.dump_excel('test2.xlsx')
+
+    def test_load(self):
+        s = Session()
+        s.load('test.h5', ['e', 'f'])
+        self.assertEqual(s.names, ['e', 'f'])
 
 if __name__ == "__main__":
     # import doctest
