89586898687a78eb076ef4f92c6749320b17cb09
Gaëtan de Menten <gdementen@gmail.com>
2015-12-01 16:35:19 +0100

    added Johan

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -61,7 +61,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'LArray'
-copyright = '2014, Gaetan de Menten, Geert Bryon'
+copyright = '2014-2015, Gaëtan de Menten, Geert Bryon, Johan Duyck'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -214,7 +214,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   ('index', 'larray.tex', 'LArray Documentation',
-   'Gaetan de Menten, Geert Bryon', 'manual'),
+   'Gaëtan de Menten, Geert Bryon, Johan Duyck', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -244,7 +244,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'larray', 'LArray Documentation',
-     ['Gaetan de Menten, Geert Bryon'], 1)
+     ['Gaëtan de Menten, Geert Bryon, Johan Duyck'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -257,8 +257,12 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'LArray', 'LArray Documentation',
-   'Gaetan de Menten, Geert Bryon', 'LArray', 'One line description of project.',
+  ('index',
+   'LArray',
+   'LArray Documentation',
+   'Gaëtan de Menten, Geert Bryon, Johan Duyck',
+   'LArray',
+   'One line description of project.',
    'Miscellaneous'),
 ]
 
