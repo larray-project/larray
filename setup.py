04341a2549e22ed1e4665e9271f43fc4decfe365
Gaëtan de Menten <gdementen@gmail.com>
2015-12-09 14:37:20 +0100

    prepare for 0.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readlocal(fname):
 
 
 DISTNAME = 'larray'
-VERSION = '0.3'
+VERSION = '0.4'
 AUTHOR = 'Gaetan de Menten, Geert Bryon'
 AUTHOR_EMAIL = 'gdementen@gmail.com'
 DESCRIPTION = "N-D labeled arrays in Python"
