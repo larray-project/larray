5fcecf2c62899ed289a39b20a0f2236f4d26a3e0
Gaëtan de Menten <gdementen@gmail.com>
2016-03-02 12:35:09 +0100

    prepare for 0.9.2

--- larray/core.py
+++ larray/core.py
@@ -1,7 +1,7 @@
 # -*- coding: utf8 -*-
 from __future__ import absolute_import, division, print_function
 
-__version__ = "0.9.1"
+__version__ = "0.9.2"
 
 __all__ = [
     'LArray', 'Axis', 'AxisCollection', 'LGroup',
