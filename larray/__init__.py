b3970f57fb3eecc7189fbe908ce5a8afa0e04f19
Gaëtan de Menten <gdementen@gmail.com>
2016-05-24 11:42:02 +0200

    comment out xw_compat, which is not ready yet

--- larray/__init__.py
+++ larray/__init__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, print_function
 from larray.core import *
 from larray.session import *
 from larray.ufuncs import *
-import larray.xw_compat
+# import larray.xw_compat
 
 try:
     import sys
