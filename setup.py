ff7aa79e43489b33268d5b2b878d34f2a2f9223e
Gaëtan de Menten <gdementen@gmail.com>
2016-05-25 15:42:48 +0200

    update version for 0.11.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readlocal(fname):
 
 
 DISTNAME = 'larray'
-VERSION = '0.11'
+VERSION = '0.11.1'
 AUTHOR = 'Gaetan de Menten, Geert Bryon'
 AUTHOR_EMAIL = 'gdementen@gmail.com'
 DESCRIPTION = "N-D labeled arrays in Python"
