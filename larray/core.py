536aaf6923da69f83fb2d2920d71de799da1e8f7
Gaëtan de Menten <gdementen@gmail.com>
2017-01-16 15:46:00 +0100

    bump to 0.19

--- larray/core.py
+++ larray/core.py
@@ -1,7 +1,7 @@
 # -*- coding: utf8 -*-
 from __future__ import absolute_import, division, print_function
 
-__version__ = "0.18"
+__version__ = "0.19"
 
 __all__ = [
     'LArray', 'Axis', 'AxisCollection', 'LGroup', 'LSet', 'PGroup',
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readlocal(fname):
 
 
 DISTNAME = 'larray'
-VERSION = '0.18'
+VERSION = '0.19'
 AUTHOR = 'Gaetan de Menten, Geert Bryon'
 AUTHOR_EMAIL = 'gdementen@gmail.com'
 DESCRIPTION = "N-D labeled arrays in Python"
