2449f471598a41833eb2f85feb3983b03131dad1
Gaëtan de Menten <gdementen@gmail.com>
2017-01-24 16:16:04 +0100

    add python 3.6 as an officially supported version

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
     'Topic :: Scientific/Engineering',
     'Topic :: Software Development :: Libraries',
 ]
