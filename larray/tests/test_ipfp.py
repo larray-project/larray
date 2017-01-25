8383c0fcca2417d11609a05c02be6e8c95d57109
Gaëtan de Menten <gdementen@gmail.com>
2017-01-25 11:28:36 +0100

    added check that ipfp target sums haves expected axes (closes #42)

--- larray/tests/test_ipfp.py
+++ larray/tests/test_ipfp.py
@@ -15,25 +15,29 @@ class TestIPFP(TestCase):
         initial = LArray([[2, 1],
                           [1, 2]], [a, b])
 
-        # sums already correct
+        # array sums already match target sums
         # [3, 3], [3, 3]
         r = ipfp([initial.sum(a), initial.sum(b)], initial)
         assert_array_equal(r, [[2, 1],
                                [1, 2]])
 
-        # different sums (ie the usual case)
+        # array sums do not match target sums (ie the usual case)
         along_a = LArray([2, 1], b)
         along_b = LArray([1, 2], a)
         r = ipfp([along_a, along_b], initial)
         assert_array_equal(r, [[0.8, 0.2],
                                [1.0, 1.0]])
 
-        # different sums, more precise threshold
-        along_a = LArray([2, 1], b)
-        along_b = LArray([1, 2], a)
+        # same as above but using a more precise threshold
         r = ipfp([along_a, along_b], initial, threshold=0.01)
         assert_array_equal(r, [[0.8450704225352113, 0.15492957746478875],
                                [1.1538461538461537, 0.8461538461538463]])
+
+        # inverted target sums
+        with self.assertRaisesRegexp(ValueError, "axes of target sum along axis 0 \(a\) do not match corresponding "
+                                                 "array axes: got {a\*} but expected {b\*}. Are the target sums in the "
+                                                 "correct order\?"):
+            ipfp([along_b, along_a], initial, threshold=0.01)
 
     def test_ipfp_no_values(self):
         # 6, 12, 18
