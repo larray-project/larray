18a6766d17de0c176693da3b16979725a816c5a6
Gaëtan de Menten <gdementen@gmail.com>
2017-01-30 12:51:01 +0100

    bump version to 0.20

--- larray/core.py
+++ larray/core.py
@@ -1,7 +1,7 @@
 # -*- coding: utf8 -*-
 from __future__ import absolute_import, division, print_function
 
-__version__ = "0.19.1"
+__version__ = "0.20"
 
 __all__ = [
     'LArray', 'Axis', 'AxisCollection', 'LGroup', 'LSet', 'PGroup',
