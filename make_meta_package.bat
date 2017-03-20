28c1f8e3a08bf4782516d40e81f23bdca5447cf0
Gaëtan de Menten <gdementen@gmail.com>
2017-03-20 16:08:20 +0100

    added larray_eurostat to larrayenv

--- make_meta_package.bat
+++ make_meta_package.bat
@@ -1,1 +1,1 @@
-conda metapackage larrayenv %1 --dependencies "larray ==%1" qtconsole matplotlib pyqt qtpy pytables xlsxwriter xlrd openpyxl xlwings
+conda metapackage larrayenv %1 --dependencies "larray ==%1" larray_eurostat qtconsole matplotlib pyqt qtpy pytables xlsxwriter xlrd openpyxl xlwings
