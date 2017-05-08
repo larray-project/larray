09f2590f0a69c18fe926fcb6b2cedfc1c053efaa
Gaëtan de Menten <gdementen@gmail.com>
2017-05-08 11:58:26 +0200

    implemented excel.Workbook.__delitem__

--- larray/tests/test_excel.py
+++ larray/tests/test_excel.py
@@ -13,9 +13,21 @@ from larray import open_excel
 
 @pytest.mark.skipif(xw is None, reason="xlwings is not available")
 class TestExcel(TestCase):
+    def test_setitem(self):
+        with open_excel(visible=False) as wb:
+            wb['sheet_name'] = 'sheet content'
+            assert wb.sheet_names() == ['sheet_name']
+
+    def test_delitem(self):
+        with open_excel(visible=False) as wb:
+            wb['sheet1'] = 'sheet1 content'
+            wb['sheet2'] = 'sheet2 content'
+            del wb['sheet1']
+            assert wb.sheet_names() == ['sheet2']
+
     def test_rename(self):
         with open_excel(visible=False) as wb:
-            # create sheet
-            wb['new'] = 'hello world'
-            wb['new'].name = 'renamed'
+            wb['sheet_name'] = 'sheet content'
+            wb['sheet_name'].name = 'renamed'
             assert wb.sheet_names() == ['renamed']
+
