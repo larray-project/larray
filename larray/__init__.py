bec3353f8fb0c3c00f1910d5f102285ec00ee233
Alix Damman <ald@plan.be>
2017-05-31 11:42:37 +0200

    added EXAMPLE_FILES_DIR to larray's namespace

--- larray/__init__.py
+++ larray/__init__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, print_function
 from larray.core import *
 from larray.io import *
 from larray.util import *
-from larray.example import load_example_data
+from larray.example import *
 from larray.extra import *
 from larray.viewer import *
 
