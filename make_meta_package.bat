9bd2329fb7e3d29f606635d74613c2dbb3294084
Alix Damman <ald@plan.be>
2017-06-13 16:41:23 +0200

    updated changelog 0.25 + make_meta_package.bat --> installing larray-editor will creates a menu LArray in the Windows Start Menu containing: - link to documentation of last stable release of larray - shortcut to launch the LArray editor - shortcut to update larrayenv

--- make_meta_package.bat
+++ make_meta_package.bat
@@ -1,1 +1,1 @@
-conda metapackage larrayenv %1 --dependencies "larray ==%1" larray_eurostat qtconsole matplotlib pyqt qtpy pytables xlsxwriter xlrd openpyxl xlwings
+conda metapackage larrayenv %1 --dependencies "larray ==%1" larray_eurostat larray-editor qtconsole matplotlib pyqt qtpy pytables xlsxwriter xlrd openpyxl xlwings
