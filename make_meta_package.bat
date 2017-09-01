6f0debb098eb0b5f2d31857a661dd48dd41ff3f8
Gaëtan de Menten <gdementen@gmail.com>
2017-09-01 12:11:24 +0200

    prepare for release on monday

--- make_meta_package.bat
+++ make_meta_package.bat
@@ -1,1 +1,1 @@
-conda metapackage larrayenv %1 --dependencies "larray ==%1" "larray-editor ==0.1" "larray_eurostat ==0.1" qtconsole matplotlib pyqt qtpy pytables xlsxwriter xlrd openpyxl xlwings
+conda metapackage larrayenv %1 --dependencies "larray ==%1" "larray-editor ==%1" "larray_eurostat ==0.1" qtconsole matplotlib pyqt qtpy pytables xlsxwriter xlrd openpyxl xlwings
