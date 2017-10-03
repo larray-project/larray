2b71f4c86768f86837a4714a6784458b7968a052
Gaëtan de Menten <gdementen@gmail.com>
2017-10-03 12:46:58 +0200

    made Session binary and unary ops tolerant to all exceptions (the result will be nan for the concerned arrays)

--- larray/core/session.py
+++ larray/core/session.py
@@ -656,7 +656,8 @@ class Session(object):
                     other_array = other.get(name, np.nan)
                     try:
                         res_array = getattr(self_array, opfullname)(other_array)
-                    except TypeError:
+                    # TypeError for str arrays, ValueError for incompatible axes, ...
+                    except Exception:
                         res_array = np.nan
                     res.append((name, res_array))
             return Session(res)
@@ -679,7 +680,7 @@ class Session(object):
                 for k, v in self.items():
                     try:
                         res_array = getattr(v, opfullname)()
-                    except TypeError:
+                    except Exception:
                         res_array = np.nan
                     res.append((k, res_array))
             return Session(res)
