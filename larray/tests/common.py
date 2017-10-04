5ab0353e23ec0f478287440f207ce2a7b9795413
Gaëtan de Menten <gdementen@gmail.com>
2017-10-04 17:14:53 +0200

    added new assert functions: assert_larray_equal, assert_larray_nan_equal, assert_larray_equiv, assert_larray_nan_equiv, assert_nparray_equal, assert_nparray_nan_equal, assert_nparray_equiv, assert_nparray_nan_equiv

--- larray/tests/common.py
+++ larray/tests/common.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division, print_function
 
 import os
 import numpy as np
-from larray import LArray
+from larray import LArray, isnan, aslarray
 
 
 TESTDATADIR = os.path.dirname(__file__)
@@ -19,7 +19,7 @@ def abspath(relpath):
 # that we know which is which) or use a class, just for that? group(a, b, c) vs family(group(a), b, c)
 
 
-def assert_equal_factory(test_func, check_shape=True, check_axes=True):
+def assert_equal_factory(test_func):
     def assert_equal(a, b):
         if isinstance(a, LArray) and isinstance(b, LArray) and a.axes != b.axes:
             raise AssertionError("axes differ:\n%s\n\nvs\n\n%s" % (a.axes.info, b.axes.info))
@@ -38,15 +38,57 @@ def assert_equal_factory(test_func, check_shape=True, check_axes=True):
     return assert_equal
 
 
+def assert_larray_equal_factory(test_func, convert=True, check_axes=False):
+    def assert_equal(a, b):
+        if convert:
+            a = aslarray(a)
+            b = aslarray(b)
+        if check_axes and a.axes != b.axes:
+            raise AssertionError("axes differ:\n%s\n\nvs\n\n%s" % (a.axes.info, b.axes.info))
+        equal = test_func(a, b)
+        if not equal.all():
+            notequal = ~equal
+            raise AssertionError("\ngot:\n\n%s\n\nexpected:\n\n%s" % (a[notequal], b[notequal]))
+    return assert_equal
+
+
+def assert_nparray_equal_factory(test_func, convert=True, check_shape=False):
+    def assert_equal(a, b):
+        if convert:
+            a = np.asarray(a)
+            b = np.asarray(b)
+        if check_shape and a.shape != b.shape:
+            raise AssertionError("shapes differ: %s != %s" % (a.shape, b.shape))
+        equal = test_func(a, b)
+        if not equal.all():
+            notequal = ~equal
+            raise AssertionError("\ngot:\n\n%s\n\nexpected:\n\n%s" % (a[notequal], b[notequal]))
+    return assert_equal
+
+
 def equal(a, b):
     return a == b
 
 
 def nan_equal(a, b):
-    return (a == b) | (np.isnan(a) & np.isnan(b))
+    return (a == b) | (isnan(a) & isnan(b))
 
 
 # numpy.testing.assert_array_equal/assert_equal would work too but it does not (as of numpy 1.10) display specifically
 # the non equal items
+# TODO: this is defined for backward compatibility only (until we update all tests to use either assert_larray* or
+#       assert_nparray*)
 assert_array_equal = assert_equal_factory(equal)
 assert_array_nan_equal = assert_equal_factory(nan_equal)
+
+assert_larray_equal = assert_larray_equal_factory(equal, check_axes=True)
+assert_larray_nan_equal = assert_larray_equal_factory(nan_equal, check_axes=True)
+
+assert_larray_equiv = assert_larray_equal_factory(equal)
+assert_larray_nan_equiv = assert_larray_equal_factory(nan_equal)
+
+assert_nparray_equal = assert_nparray_equal_factory(equal, check_shape=True)
+assert_nparray_nan_equal = assert_nparray_equal_factory(nan_equal, check_shape=True)
+
+assert_nparray_equiv = assert_nparray_equal_factory(equal)
+assert_nparray_nan_equiv = assert_nparray_equal_factory(nan_equal)
