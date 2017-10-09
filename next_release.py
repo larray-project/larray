6a8c4aaf60754735bc3cd41ea265b96fbe29ab25
Gaëtan de Menten <gdementen@gmail.com>
2017-10-09 12:34:20 +0200

    changed next_release.py to include "current" release changes in the changelog (backported from LIAM2 corresponding script)

--- next_release.py
+++ next_release.py
@@ -1,18 +1,44 @@
 #!/usr/bin/python
-# coding=utf-8
+# encoding: utf-8
 # script to start a new release cycle
 # Licence: GPLv3
 from os.path import join
-from make_release import relname2fname
+from make_release import relname2fname, short
+from shutil import copy
 
 
 def add_release(release_name):
-    # create "empty" change file for that release
     fname = relname2fname(release_name)
-    with open(r'doc\source\changes\template.rst.inc') as f:
-        changes_template = f.read()
-    with open(join(r'doc\source\changes', fname), 'w') as f:
-        f.write(changes_template)
+
+    # create "empty" changelog for that release
+    changes_dir = r'doc\source\changes'
+    copy(join(changes_dir, 'template.rst.inc'),
+         join(changes_dir, fname))
+
+    # include release changelog in changes.rst
+    fpath = r'doc\source\changes.rst'
+    changelog_index_template = """{title}
+{underline}
+
+In development.
+
+.. include:: {fpath}
+
+
+"""
+
+    with open(fpath) as f:
+        lines = f.readlines()
+        title = "Version %s" % short(release_name)
+        if lines[3] == title + '\n':
+            print("changes.rst not modified (it already contains %s)" % title)
+            return
+        this_version = changelog_index_template.format(title=title,
+                                                       underline="=" * len(title),
+                                                       fpath='./changes/' + fname)
+        lines[3:3] = this_version.splitlines(True)
+    with open(fpath, 'w') as f:
+        f.writelines(lines)
 
 
 if __name__ == '__main__':
