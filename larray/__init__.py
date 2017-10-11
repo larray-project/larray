b23e8cc079c04dc71e3b1270c4645f594ede9484
Gaëtan de Menten <gdementen@gmail.com>
2017-10-11 15:52:02 +0200

    renamed larray.core.abc to abstractbases and larray.io to larray.inout
    I was fed up with pytest/pycharm being confused by those and having to fiddle with settings to run tests

--- larray/__init__.py
+++ larray/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
 from larray.core import *
-from larray.io import *
+from larray.inout import *
 from larray.util import *
 from larray.example import *
 from larray.extra import *
---
+++ larray/inout/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import, division, print_function
+
+from larray.inout.excel import open_excel
+from larray.inout.array import *
+from larray.inout.session import *
