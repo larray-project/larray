4a398decdbd251e5e5601fec5740194b22bedd02
Alix Damman <ald@plan.be>
2018-03-16 16:30:30 +0100

    bump version to 0.29-dev

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readlocal(fname):
 
 
 DISTNAME = 'larray'
-VERSION = '0.28'
+VERSION = '0.29-dev'
 AUTHOR = 'Gaetan de Menten, Geert Bryon, Johan Duyck, Alix Damman'
 AUTHOR_EMAIL = 'gdementen@gmail.com'
 DESCRIPTION = "N-D labeled arrays in Python"
