938716597e53187f117ee880d96185d5517d80ab
Alix Damman <ald@plan.be>
2018-04-09 10:10:10 +0200

    fix #578 : allowed to save and load all Axis and Group objects of a session in/from HDF, CSV and EXCEL files

--- larray/inout/pickle.py
+++ larray/inout/pickle.py
@@ -1,27 +1,51 @@
 from __future__ import absolute_import, division, print_function
 
+import os.path
 from collections import OrderedDict
 
+from larray.core.axis import Axis
+from larray.core.group import Group
+from larray.core.array import LArray
 from larray.util.misc import pickle
+from larray.inout.session import register_file_handler
 from larray.inout.common import FileHandler
 
 
+__all__ = []
+
+
+@register_file_handler('pickle', ['pkl', 'pickle'])
 class PickleHandler(FileHandler):
     def _open_for_read(self):
         with open(self.fname, 'rb') as f:
-            self.data = pickle.load(f)
+            self.data = OrderedDict(pickle.load(f))
 
     def _open_for_write(self):
-        self.data = OrderedDict()
+        if not self.overwrite_file and os.path.isfile(self.fname):
+            self._open_for_read()
+        else:
+            self.data = OrderedDict()
 
-    def list(self):
-        return self.data.keys()
+    def list_items(self):
+        # axes
+        items = [(key, 'Axis') for key, value in self.data.items() if isinstance(value, Axis)]
+        # groups
+        items += [(key, 'Group') for key, value in self.data.items() if isinstance(value, Group)]
+        # arrays
+        items += [(key, 'Array') for key, value in self.data.items() if isinstance(value, LArray)]
+        return items
 
-    def _read_item(self, key):
-        return key, self.data[key]
+    def _read_item(self, key, type, *args, **kwargs):
+        if type in ['Array', 'Axis', 'Group']:
+            return key, self.data[key]
+        else:
+            raise TypeError()
 
-    def _dump(self, key, value):
-        self.data[key] = value
+    def _dump_item(self, key, value, *args, **kwargs):
+        if isinstance(value, (LArray, Axis, Group)):
+            self.data[key] = value
+        else:
+            raise TypeError()
 
     def close(self):
         with open(self.fname, 'wb') as f:
