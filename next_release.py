0441c98de10438b29d44822b7299090a07dc7dc4
Gaëtan de Menten <gdementen@gmail.com>
2019-11-12 17:19:07 +0100

    removed (unused) branch argument from next_release.add_release()

--- next_release.py
+++ next_release.py
@@ -11,8 +11,8 @@ if __name__ == '__main__':
 
     argv = sys.argv
     if len(argv) < 2:
-        print(f"Usage: {argv[0]} release_name [branch]")
+        print(f"Usage: {argv[0]} release_name")
         sys.exit()
 
     local_repository = abspath(dirname(__file__))
-    add_release(local_repository, PACKAGE_NAME, SRC_CODE, *argv[1:], src_documentation=SRC_DOC)
+    add_release(local_repository, PACKAGE_NAME, SRC_CODE, release_name=argv[1], src_documentation=SRC_DOC)
