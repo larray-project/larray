688ef5350831f89f27f73391783da804c4a73241
Gaëtan de Menten <gdementen@gmail.com>
2020-03-30 18:25:04 +0200

    added support for Python 3.8 (closes #850)

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
     'Topic :: Scientific/Engineering',
     'Topic :: Software Development :: Libraries',
 ]
