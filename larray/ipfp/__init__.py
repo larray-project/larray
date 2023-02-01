329f2acdc1f72a266b68ab7bcf516e64f8d7d4a6
Gaëtan de Menten <gdementen@gmail.com>
2023-02-01 12:28:46 +0100

    MAINT: changed "ipfp package moved" warning into an exception
    this also fixed a ruff warning

--- larray/ipfp/__init__.py
+++ larray/ipfp/__init__.py
@@ -1,6 +1,2 @@
-import warnings
-
-import larray.extra.ipfp as ipfp
-
-warnings.warn('ipfp function should be imported as "from larray import ipfp" or not imported explicitly at all if you '
-              'use "from larray import *"', FutureWarning, stacklevel=2)
+raise ImportError('ipfp function should be imported as "from larray import ipfp" or not imported explicitly at all '
+                  'if you use "from larray import *"')
