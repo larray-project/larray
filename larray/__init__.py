cb82b7fb8df70df523c0e592e46fb5e72744c028
Gaëtan de Menten <gdementen@gmail.com>
2023-09-14 13:38:23 +0200

    bump version to 0.34.1

--- larray/__init__.py
+++ larray/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.34.1-dev'
+__version__ = '0.34.1'
 
 
 from larray.core.axis import Axis, AxisCollection, X
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readlocal(fname):
 
 
 DISTNAME = 'larray'
-VERSION = '0.34.1-dev'
+VERSION = '0.34.1'
 AUTHOR = 'Gaetan de Menten, Geert Bryon, Johan Duyck, Alix Damman'
 AUTHOR_EMAIL = 'gdementen@gmail.com'
 DESCRIPTION = "N-D labeled arrays in Python"
