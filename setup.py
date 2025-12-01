539a06b7ada8ad12735a62bc2c8893452874faf6
Gaëtan de Menten <gdementen@gmail.com>
2025-12-01 10:17:19 +0100

    MAINT: added support for Python 3.13 (closes #1128)

--- setup.py
+++ setup.py
@@ -30,11 +30,11 @@ CLASSIFIERS = [
     'Intended Audience :: Developers',
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.11',
     'Programming Language :: Python :: 3.12',
+    'Programming Language :: Python :: 3.13',
     'Topic :: Scientific/Engineering',
     'Topic :: Software Development :: Libraries',
 ]
